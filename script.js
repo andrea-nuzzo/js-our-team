b9ba71e1d492316a23b2746ff9f6954c30d7f05e
Andrea <57769818+andrea-nuzzo@users.noreply.github.com>
2021-11-19 15:51:35 +0100

    implemented a function that creates the cards

--- script.js
+++ script.js
@@ -32,28 +32,61 @@ const teamCard = [
    
 ];
 
-const teamContainer = document.querySelector('.team-container');
 
+
+
+// Questa variabile serrve per il posityionamento delle Card in Html
+const position = 'team-container'
+
+
+// Questo ciclo popola il container di card
 for( let i = 0; i < teamCard.length; i++){
-
-    teamContainer.innerHTML += 
-    `<div class="team-card">
-     <div class="card-image">
-        <img
-          src=${teamCard[i].image}
-          alt=${teamCard[i].name}
-        />
-      </div>
-      <div class="card-text">
-        <h3>${teamCard[i].name}</h3>
-        <p>${teamCard[i].role}</p>
-      </div>
-      </div>`;
-
+    createCard(teamCard[i].image, teamCard[i].name,teamCard[i].role, position);
 }
 
 
-function createCard(image, name, role){
+
+
+// Questa funzione crea le card nella posizione desiderata.
+function createCard(image, fullName, role, classWhereAppendCard){
+
+    const position = document.querySelector('.' + classWhereAppendCard);
+
+    // Creo il div Team-Card
+    const teamCard = document.createElement('div');
+    teamCard.classList.add("team-card");
+    position.appendChild(teamCard);
     
+    // Creo il div Card Image
+    const cardImage = document.createElement('div');
+    cardImage.classList.add("card-image");
 
+    // Creo l'immagine
+    const img = document.createElement('img');
+    img.src = image;
+    img.alt = fullName;
+    
+    // Creo il div Card Text
+    const cardText = document.createElement('div');
+    cardText.classList.add("card-text");
+    console.log(cardText);
+
+    // Creo h3 nome
+    const name = document.createElement('h3');
+    const insertName = document.createTextNode(fullName);
+    name.appendChild(insertName);
+
+    //Creo il p 
+    const jobRole = document.createElement('p');
+    const insertJobrole = document.createTextNode(role);
+    jobRole.appendChild(insertJobrole);
+
+
+    //Uniamo tutti gli elementi della Card
+    teamCard.appendChild(cardImage, cardText);
+    cardImage.appendChild(img);
+    teamCard.appendChild(cardText);
+    cardText.appendChild(name);
+    cardText.appendChild(jobRole);
 }
+
