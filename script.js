ff3efff4f4c52b04686e3dca92bb8e6ff921b789
Andrea <57769818+andrea-nuzzo@users.noreply.github.com>
2021-11-19 14:38:18 +0100

    created an array of objects

---
+++ script.js
@@ -0,0 +1,33 @@
+const teamCard = [
+    {
+        'image':'img/wayne-barnett-founder-ceo.jpg',
+        'name':'Wayne Barnet',
+        'role':'Founder & CEO',
+    },
+    {
+        'image':'img/angela-caroll-chief-editor.jpg',
+        'name':'Angela Caroll',
+        'role':'Chief Editor',
+    },
+    {
+        'image':'img/walter-gordon-office-manager.jpg',
+        'name':'Walter Gordon',
+        'role':'Office Manager',
+    },
+    {
+        'image':'img/angela-lopez-social-media-manager.jpg',
+        'name':'Angela Lopez',
+        'role':'Social Media Manager',
+    },
+    {
+        'image':'img/scott-estrada-developer.jpg',
+        'name':'Scott Estrada',
+        'role':'Developer',
+    },
+    {
+        'image':'img/barbara-ramos-graphic-designer.jpg',
+        'name':'Barbara Ramos',
+        'role':'Office Manager',
+    },
+   
+];
